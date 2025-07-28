d14c25e5d9b8484af78b6ec625cc93c1ef35e400
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2025-07-28 11:06:18 +0200

    feat: improve astronvim4

--- pkgs/astronvim4/default.nix
+++ pkgs/astronvim4/default.nix
@@ -6,69 +6,21 @@
   nvim-appname ? "astronvim4",
   vimPlugins,
   writeShellScriptBin,
-}:
-writeShellScriptBin "astronvim4" ''
-  set -efu
-
-  # Remove VIMINIT variable
-  unset VIMINIT
-
-  # Parse command line arguments
-  NVIM_APPNAME="${nvim-appname}"
-  NO_LINK=0
-  ARGS=()
-  while [[ $# -gt 0 ]]; do
-    case $1 in
-      --app-name)
-        NVIM_APPNAME="$2"
-        shift 2
-        ;;
-      --no-link)
-        NO_LINK=1
-        shift
-        ;;
-      *)
-        ARGS+=("$1")
-        shift
-        ;;
-    esac
-  done
-
-  export PATH=${buildEnv {
-    name = "lsp-servers";
-    paths = astronvim4-config.lspToolingPkgs;
-  }}/bin:$PATH
-  export NVIM_APPNAME
-
-  # Ensure XDG folders
-  XDG_CONFIG_HOME=''${XDG_CONFIG_HOME:-$HOME/.config}
-  XDG_DATA_HOME=''${XDG_DATA_HOME:-$HOME/.local/share}
-  mkdir -p "$XDG_CONFIG_HOME" "$XDG_DATA_HOME"
-
-  # Create nvim variables
-  NVIM_CONFIG="$XDG_CONFIG_HOME/$NVIM_APPNAME"
-  NVIM_DATA="$XDG_DATA_HOME/$NVIM_APPNAME"
-
-  # Link or copy custom config based on --no-link option
-  if [ $NO_LINK -eq 1 ]; then
-    rm -rf "$NVIM_CONFIG"
-    cp -R -L ${astronvim4-config} "$NVIM_CONFIG"
-    chown -R $(id -u):$(id -g) $NVIM_CONFIG
-    chmod -R u=rwX,go=rX "$NVIM_CONFIG"
-  else
-    ln -sfT ${astronvim4-config} "$NVIM_CONFIG"
-  fi
-
-
-  # Install plugins and necessary tooling
-  ${lib.getExe neovim} --headless -c 'quitall'
-
-  # Link telescope-fzf-native plugin
-  if [[ -d $NVIM_DATA/lazy/telescope-fzf-native.nvim ]]; then
-    mkdir -p "$NVIM_DATA/lazy/telescope-fzf-native.nvim/build"
-    ln -sf "${vimPlugins.telescope-fzf-native-nvim}/build/libfzf.so" "$NVIM_DATA/lazy/telescope-fzf-native.nvim/build/libfzf.so"
-  fi
-
-  # Run nvim
-  exec ${lib.getExe neovim} "''${ARGS[@]}"
-''
+}: let
+  common = import ./lib.nix {
+    inherit lib neovim buildEnv writeShellScriptBin;
+  };
+in
+  common.mkAstronvimWrapper {
+    name = "astronvim4";
+    config = astronvim4-config;
+    inherit nvim-appname;
+    postInit = ''
+      # Link telescope-fzf-native plugin (v4 specific)
+      if [[ -d $NVIM_DATA/lazy/telescope-fzf-native.nvim ]]; then
+        [[ $VERBOSE -eq 1 ]] && echo "Linking telescope-fzf-native binary..."
+        mkdir -p "$NVIM_DATA/lazy/telescope-fzf-native.nvim/build"
+        ln -sf "${vimPlugins.telescope-fzf-native-nvim}/build/libfzf.so" "$NVIM_DATA/lazy/telescope-fzf-native.nvim/build/libfzf.so"
+      fi
+    '';
+  }
