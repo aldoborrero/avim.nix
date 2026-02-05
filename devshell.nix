22abff1d03041a762ec69433074575630d52dffd
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2026-02-05 09:49:05 +0000

    feat: simplify devshell

--- devshell.nix
+++ devshell.nix
@@ -1,29 +1,9 @@
 {
   pkgs,
-  perSystem,
   ...
 }:
-perSystem.devshell.mkShell {
-  name = "astronvim.nix";
-  packages = [ ];
-  env = [
-    {
-      name = "NIX_PATH";
-      eval = "nixpkgs=${pkgs.path}";
-    }
-  ];
-  commands = [
-    {
-      name = "fmt";
-      category = "Formatters";
-      help = "Format the source tree";
-      command = ''nix fmt'';
-    }
-    {
-      name = "clean";
-      category = "Utils";
-      help = "Cleans any result produced by Nix or associated tools";
-      command = "rm -rf result* *.qcow2 *.fd";
-    }
-  ];
+pkgs.mkShellNoCC {
+  shellHook = ''
+    export NIX_PATH="nixpkgs=${pkgs.path}"
+  '';
 }
